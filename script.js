1813f559255dd2d64900ce52a8cd7a30dec64d0c
RicardoFernandes2004 <Ricardo.Fernandes02082004@gmail.com>
2025-07-31 02:34:27 -0300

    updated script.js to fix viewport meta tag and URL shortening functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function encurtar() {
   const url = document.getElementById("urlInput").value;
-  const response = await fetch("http://localhost:7000/api/urls/posturl", {
+  const response = await fetch("https://url-shortener-ysr1.onrender.com/api/urls/posturl", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -10,7 +10,7 @@ async function encurtar() {
 
   if (response.ok) {
     const data = await response.json();
-        const encurtada = `http://localhost:7000/api/urls/${data.id}`;
+        const encurtada = `https://url-shortener-ysr1.onrender.com/api/urls/${data.id}`;
         document.getElementById("resultado").innerHTML = `
      URL encurtada: <a href="${encurtada}" target="_blank">${encurtada}</a>
     `;
